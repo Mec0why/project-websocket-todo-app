d939170640a866f341b9f4c159ed79106f1dfa6b
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-17 19:40:44 +0200

    Add server-side Socket updateData

--- server.js
+++ server.js
@@ -27,4 +27,12 @@ const io = socket(server);
 
 io.on('connection', (socket) => {
   console.log('New client! Its id – ' + socket.id);
+
+  socket.on('updateData', () => {
+    socket.emit('updateData', db.tasks);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Oh, socket ' + socket.id + ' has left');
+  });
 });
