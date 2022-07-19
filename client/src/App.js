7e8e11ba3bee89242d14490f7658fd87afff81d6
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-19 11:52:30 +0200

    Remove static data in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,12 +27,6 @@ const App = () => {
               {task.name} <button className='btn btn--red'>Remove</button>
             </li>
           ))}
-          <li className='task'>
-            Shopping <button className='btn btn--red'>Remove</button>
-          </li>
-          <li className='task'>
-            Go out with a dog <button className='btn btn--red'>Remove</button>
-          </li>
         </ul>
 
         <form id='add-task-form'>
