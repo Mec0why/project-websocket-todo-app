069320bf5adbc959aedd60a0a07e6df526548a93
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-19 18:09:18 +0200

    BUG - Cannot remove the task from other list

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,50 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+import shortid from 'shortid';
+
 const serverURL = 'http://localhost:8000/';
 
 const App = () => {
   const [socket] = useState(io(serverURL));
   const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
 
   useEffect(() => {
     socket.on('updateData', (tasks) => {
-      setTasks(tasks);
+      updateTasks(tasks);
     });
 
-    socket.on('removeTask', (tasks) => {
-      setTasks(tasks);
+    socket.on('removeTask', (taskId) => {
+      removeTask(taskId, false);
+    });
+
+    socket.on('addTask', ({ id, name }) => {
+      addTask({ id, name });
     });
   }, []);
 
-  const removeTask = (taskId) => {
+  const addTask = (task) => {
+    setTasks((tasks) => [...tasks, task]);
+    setTaskName('');
+  };
+
+  console.log(tasks);
+
+  const updateTasks = (tasksData) => {
+    setTasks(tasksData);
+  };
+
+  const removeTask = (taskId, isLocal) => {
     setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
-    socket.emit('removeTask', taskId);
+    if (isLocal) {
+      socket.emit('removeTask', taskId);
+    }
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    addTask({ id: shortid.generate(), name: taskName });
+    socket.emit('addTask', { id: shortid.generate(), name: taskName });
   };
 
   return (
@@ -33,9 +59,9 @@ const App = () => {
         <ul className='tasks-section__list' id='tasks-list'>
           {tasks.map((task) => (
             <li key={task.id} className='task'>
-              {task.name}{' '}
+              {task.name}
               <button
-                onClick={() => removeTask(task.id)}
+                onClick={() => removeTask(task.id, true)}
                 className='btn btn--red'
               >
                 Remove
@@ -44,13 +70,15 @@ const App = () => {
           ))}
         </ul>
 
-        <form id='add-task-form'>
+        <form id='add-task-form' onSubmit={submitForm}>
           <input
             className='text-input'
             autoComplete='off'
             type='text'
             placeholder='Type your description'
             id='task-name'
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
           />
           <button className='btn' type='submit'>
             Add
